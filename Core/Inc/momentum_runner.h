63f1fa70de9f74aa9c9d03ce9eb40a6a7f0c314a
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-05-17 22:39:18 -0400

    Add WIP high level momentum drivers

---
+++ Core/Inc/momentum_runner.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * @file momentum_runner.h
+ * @brief Momentum runner handling low level interacting functions.
+ *******************************************************************************
+ */
+
+#ifndef MOMENTUM__RUNNER_H
+#define MOMENTUM__RUNNER_H
+
+/** Includes. *****************************************************************/
+
+#include "momentum_driver.h"
+#include "stm32l4xx_hal.h"
+
+/** STM32 port and pin configs. ***********************************************/
+
+extern SPI_HandleTypeDef hspi3;
+
+// SPI.
+#define MOMENTUM_HSPI hspi3
+// CS pin is handled by hardware NSS.
+
+/** Public functions. *********************************************************/
+
+void HAL_SPI_TxRxCpltCallback_momentum(SPI_HandleTypeDef *hspi);
+
+#endif
