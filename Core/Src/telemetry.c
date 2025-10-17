a13bccb25cf5c84cad2cf7681c9b184a35a33f0c
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-10-17 15:40:00 -0400

    Cleanup code

--- Core/Src/telemetry.c
+++ Core/Src/telemetry.c
@@ -16,13 +16,13 @@
 /** Public functions. *********************************************************/
 
 void can_tx_state(void) {
-  can_message_t state_msg = dbc_messages[0];
-  uint32_t state_sigs[1] = {0}; // TODO: Hardcoded state.
+  const can_message_t state_msg = dbc_messages[0];
+  const uint32_t state_sigs[1] = {0}; // TODO: Hardcoded state.
   can_send_message_raw32(&hcan1, &state_msg, state_sigs);
 }
 
 void can_tx_barometric(void) {
-  can_message_t pressure_msg = dbc_messages[1];
+  const can_message_t pressure_msg = dbc_messages[1];
   uint32_t pressure_sigs[3] = {0};
   const float pressure_source_sigs[3] = {bmp390_pressure, bmp390_temperature,
                                          (float)0}; // TODO: Hardcoded state.
@@ -34,7 +34,7 @@ void can_tx_barometric(void) {
 }
 
 void can_tx_gps1(void) {
-  can_message_t gps1_msg = dbc_messages[2];
+  const can_message_t gps1_msg = dbc_messages[2];
   uint32_t gps1_sigs[2] = {0};
   const float gps1_source_sigs[2] = {gps_data.latitude, gps_data.longitude};
   for (int i = 0; i < gps1_msg.signal_count; ++i) {
@@ -44,7 +44,7 @@ void can_tx_gps1(void) {
 }
 
 void can_tx_gps2(void) {
-  can_message_t gps2_msg = dbc_messages[3];
+  const can_message_t gps2_msg = dbc_messages[3];
   uint32_t gps2_sigs[5] = {0};
   const float gps2_source_sigs[5] = {gps_data.speed_knots, gps_data.course_deg,
                                      gps_data.position_fix, gps_data.satellites,
@@ -56,7 +56,7 @@ void can_tx_gps2(void) {
 }
 
 void can_tx_gps3(void) {
-  can_message_t gps3_msg = dbc_messages[4];
+  const can_message_t gps3_msg = dbc_messages[4];
   uint32_t gps3_sigs[3] = {0};
   const float gps3_source_sigs[3] = {gps_data.altitude_m, gps_data.geoid_sep_m,
                                      (float)0}; // TODO: Hardcoded state.
@@ -67,7 +67,7 @@ void can_tx_gps3(void) {
 }
 
 void can_tx_imu1(void) {
-  can_message_t imu1_msg = dbc_messages[5];
+  const can_message_t imu1_msg = dbc_messages[5];
   uint32_t imu1_sigs[4] = {0};
   const float imu1_source_sigs[4] = {bno085_quaternion_i, bno085_quaternion_j,
                                      bno085_quaternion_k,
@@ -79,7 +79,7 @@ void can_tx_imu1(void) {
 }
 
 void can_tx_imu2(void) {
-  can_message_t imu2_msg = dbc_messages[6];
+  const can_message_t imu2_msg = dbc_messages[6];
   uint32_t imu2_sigs[3] = {0};
   const float imu2_source_sigs[3] = {bno085_gyro_x, bno085_gyro_y,
                                      bno085_gyro_z};
@@ -90,7 +90,7 @@ void can_tx_imu2(void) {
 }
 
 void can_tx_imu3(void) {
-  can_message_t imu3_msg = dbc_messages[7];
+  const can_message_t imu3_msg = dbc_messages[7];
   uint32_t imu3_sigs[3] = {0};
   const float imu3_source_sigs[3] = {bno085_accel_x, bno085_accel_y,
                                      bno085_accel_z};
@@ -101,7 +101,7 @@ void can_tx_imu3(void) {
 }
 
 void can_tx_imu4(void) {
-  can_message_t imu4_msg = dbc_messages[8];
+  const can_message_t imu4_msg = dbc_messages[8];
   uint32_t imu4_sigs[3] = {0};
   const float imu4_source_sigs[3] = {bno085_lin_accel_x, bno085_lin_accel_y,
                                      bno085_lin_accel_z};
@@ -112,7 +112,7 @@ void can_tx_imu4(void) {
 }
 
 void can_tx_imu5(void) {
-  can_message_t imu5_msg = dbc_messages[9];
+  const can_message_t imu5_msg = dbc_messages[9];
   uint32_t imu5_sigs[3] = {0};
   const float imu5_source_sigs[3] = {bno085_gravity_x, bno085_gravity_y,
                                      bno085_gravity_z};
@@ -123,13 +123,13 @@ void can_tx_imu5(void) {
 }
 
 void can_tx_rtc(void) {
-  can_message_t rtc_msg = dbc_messages[11];
+  const can_message_t rtc_msg = dbc_messages[11];
   // Get the date and time.
   RTC_DateTypeDef date;
   RTC_TimeTypeDef time;
   HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
   HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
-  uint32_t rtc_sigs[8] = {
+  const uint32_t rtc_sigs[8] = {
       0,          date.Year,    date.Month,  date.Date, date.WeekDay,
       time.Hours, time.Minutes, time.Seconds}; // TODO: Hardcoded state.
   can_send_message_raw32(&hcan1, &rtc_msg, rtc_sigs);
